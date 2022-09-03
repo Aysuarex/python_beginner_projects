a64265c54cfe2094155f1e13af9b2e87c66a4882
Aysuarex <aysuarex@gmail.com>
2022-09-03 11:29:54 +0100

    changed list variable name

--- binary_search.py
+++ binary_search.py
@@ -10,54 +10,55 @@ We will prove that binary search is faster than naive search!
 # If you have a sorted list and you want to search this array for something,
 # You could go through each item in the list and ask, is this equal to what we're looking for?
 # But we can make this *faster* by leveraging the fact that our array is sorted!
-# Binary search ~ O(log(n)), naive search ~ O(n)
+# Binary search ~ O(log(n)), Naive search ~ O(n)
 
-# In these two examples, l is a list in ascending order, and target is something that we're looking for
+# In these two examples, a_list is a list in ascending order, and target is something that we're looking for
 # Return -1 if not found
 
 
 # naive search: scan entire list and ask if its equal to the target
 # if yes, return the index
 # if no, then return -1
-def naive_search(l, target):
+def naive_search(a_list, target):
     # example l = [1, 3, 10, 12]
-    for i in range(len(l)):
-        if l[i] == target:
+    for i in range(len(a_list)):
+        if a_list[i] == target:
             return i
     return -1
 
 
 # binary search uses divide and conquer!
 # we will leverage the fact that our list is SORTED
-def binary_search(l, target, low=None, high=None):
+def binary_search(a_list, target, low=None, high=None):
     if low is None:
         low = 0
     if high is None:
-        high = len(l) - 1
+        high = len(a_list) - 1
 
     if high < low:
         return -1
 
     # example l = [1, 3, 5, 10, 12]  # should return 3
-    midpoint = (low + high) // 2  # 2
+    midpoint = (low + high) // 2
 
-    # we'll check if l[midpoint] == target, and if not, we can find out if
+    # we'll check if a_list[midpoint] == target, and if not, we can find out if
     # target will be to the left or right of midpoint
     # we know everything to the left of midpoint is smaller than the midpoint
     # and everything to the right is larger
-    if l[midpoint] == target:
+    if a_list[midpoint] == target:
         return midpoint
-    elif target < l[midpoint]:
-        return binary_search(l, target, low, midpoint-1)
+    elif target < a_list[midpoint]:
+        return binary_search(a_list, target, low, midpoint-1)
     else:
-        # target > l[midpoint]
-        return binary_search(l, target, midpoint+1, high)
+        # target > a_list[midpoint]
+        return binary_search(a_list, target, midpoint+1, high)
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     # l = [1, 3, 5, 10, 12]
     # target = 7
-    # print(naive_search(l, target))
-    # print(binary_search(l, target))
+    # print(naive_search(a_list, target))
+    # print(binary_search(a_list, target))
 
     length = 10000
     # build a sorted list of length 10000
